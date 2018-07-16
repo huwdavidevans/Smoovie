8398d09a2f698f2fff602edb408d2996f7872cb3
huwman <hevans@sorensonmedia.com>
2018-07-16 15:30:48 +0100

    switch to scss and add namespace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
