7634e3903ddecb2031797a18603ad914420531e0
huwman <hevans@sorensonmedia.com>
2018-07-16 16:54:01 +0100

    boilerplate api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from './movie.service';
 
 @Component({
   selector: 'he-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Smoovie';
+
+  constructor(private movieService: MovieService) {
+  }
+
+  ngOnInit() {
+    this.movieService.getMovies();
+  }
+
 }
