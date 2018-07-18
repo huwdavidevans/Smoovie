4064e0846b336e68cfeb349fa26b2a7b36904d99
huwman <hevans@sorensonmedia.com>
2018-07-18 16:59:53 +0100

    finally got a test in

--- src/app/actor/actor.component.spec.ts
+++ src/app/actor/actor.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActorComponent } from './actor.component';
+import { TopBarComponent } from '../top-bar/top-bar.component';
+import { SpinnerComponent } from '../spinner/spinner.component';
+import { ErrorStateComponent } from '../error-state/error-state.component';
+import { MomentModule } from 'angular2-moment';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('ActorComponent', () => {
   let component: ActorComponent;
@@ -8,7 +15,18 @@ describe('ActorComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ActorComponent ]
+      declarations: [
+        ActorComponent,
+        TopBarComponent,
+        SpinnerComponent,
+        ErrorStateComponent,
+      ], imports: [
+        BrowserModule,
+        RouterModule.forRoot([]),
+        MomentModule
+      ], providers: [
+        { provide: APP_BASE_HREF, useValue: '/'}
+      ]
     })
     .compileComponents();
   }));
--- src/app/tmdb-api.service.spec.ts
+++ src/app/tmdb-api.service.spec.ts
@@ -1,15 +1,96 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { Observable } from 'rxjs';
 import { ApiService } from './tmdb-api.service';
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from '../environments/environment.prod';
+
+
+
+
 describe('ApiService', () => {
+  let httpMock;
+  let apiService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
       providers: [ApiService]
     });
+
+    apiService = TestBed.get(ApiService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   it('should be created', inject([ApiService], (service: ApiService) => {
     expect(service).toBeTruthy();
   }));
+
+
+  describe('searchMovies', () => {
+
+    it('should return a result', (done) => {
+
+      const query = 'star wars';
+      const filter = 'both';
+      const page = 1;
+
+      const expectedUrl = 'https://api.themoviedb.org/3/search/multi?'
+        + 'include_adult=false&page=1&query=star%20wars&language=en-US'
+        + '&api_key=224cff7f326dbbfde49d42869c0e1f1e';
+
+      const expectedPayload = {
+        page: 1,
+        results: [
+          { title: 'Star Wars' }
+        ]
+      };
+
+      apiService.searchMovies(query, filter, page)
+        .subscribe((data) => {
+          expect(data).toBe(expectedPayload);
+          done();
+        });
+
+      const apiRequest = httpMock.expectOne({ method: 'GET', url: expectedUrl });
+      apiRequest.flush(expectedPayload);
+      httpMock.verify();
+
+    });
+
+  });
+
+  describe('getPopularMovies', () => {
+
+    it('should return an observable of type Object', () => { });
+  });
+
+  describe('getMovieById', () => {
+
+    it('should return an observable of type Object', () => { });
+  });
+
+  describe('getActorById', () => {
+
+    it('should return an observable of type Object', () => { });
+  });
+
+  describe('getCreditsByMovieId', () => {
+
+    it('should return an observable of type Object', () => { });
+  });
+
+  describe('getCreditsByActorId', () => {
+
+    it('should return an observable of type Object', () => { });
+  });
+
+  describe('getSimilarMoviesByMovieId', () => {
+
+    it('should return an observable of type Object', () => { });
+  });
+
 });
