61d8894bc387d501dd0c340019e12ec2d3130fd2
huwman <hevans@sorensonmedia.com>
2018-07-18 22:15:16 +0100

    all green

--- src/app/search-result/search-result.component.spec.ts
+++ src/app/search-result/search-result.component.spec.ts
@@ -1,5 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 import { SearchResultComponent } from './search-result.component';
 
 describe('SearchResultComponent', () => {
@@ -8,7 +11,14 @@ describe('SearchResultComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchResultComponent ]
+      declarations: [
+        SearchResultComponent,
+      ], imports: [
+        BrowserModule,
+        RouterModule.forRoot([])
+      ], providers: [
+        { provide: APP_BASE_HREF, useValue: '/'}
+      ]
     })
     .compileComponents();
   }));
--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { SearchBarComponent } from '../search-bar/search-bar.component';
+import { SearchResultComponent } from '../search-result/search-result.component';
+import { SpinnerComponent } from '../spinner/spinner.component';
+import { ErrorStateComponent } from '../error-state/error-state.component';
+import { MoviePosterComponent } from '../movie-poster/movie-poster.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SearchComponent } from './search.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
@@ -8,7 +19,22 @@ describe('SearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      declarations: [
+        SearchBarComponent,
+        SearchResultComponent,
+        SearchComponent,
+        SpinnerComponent,
+        ErrorStateComponent,
+        MoviePosterComponent
+      ], imports: [
+        BrowserModule,
+        RouterModule.forRoot([]),
+        FormsModule,
+        HttpClientTestingModule,
+        NgxPaginationModule
+      ], providers: [
+        { provide: APP_BASE_HREF, useValue: '/'}
+      ]
     })
     .compileComponents();
   }));
