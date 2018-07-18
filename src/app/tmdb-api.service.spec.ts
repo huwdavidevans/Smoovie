782b6dd067f05662d392cfa8ef83b06a9dc91e51
huwman <hevans@sorensonmedia.com>
2018-07-18 21:06:31 +0100

    passing tests

--- src/app/tmdb-api.service.spec.ts
+++ src/app/tmdb-api.service.spec.ts
@@ -6,9 +6,6 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { environment } from '../environments/environment.prod';
 
-
-
-
 describe('ApiService', () => {
   let httpMock;
   let apiService;
@@ -32,8 +29,16 @@ describe('ApiService', () => {
 
   describe('searchMovies', () => {
 
-    it('should return a result', (done) => {
+    let searchPayload;
 
+    beforeEach(() => {
+      searchPayload = {
+        page: 1,
+        results: [ { title: 'Star Wars' }  ]
+      };
+    });
+
+    it('should return a result for multi search', (done) => {
       const query = 'star wars';
       const filter = 'both';
       const page = 1;
@@ -42,23 +47,93 @@ describe('ApiService', () => {
         + 'include_adult=false&page=1&query=star%20wars&language=en-US'
         + '&api_key=224cff7f326dbbfde49d42869c0e1f1e';
 
-      const expectedPayload = {
-        page: 1,
-        results: [
-          { title: 'Star Wars' }
-        ]
-      };
-
       apiService.searchMovies(query, filter, page)
         .subscribe((data) => {
-          expect(data).toBe(expectedPayload);
+          expect(data).toBe(searchPayload);
           done();
         });
 
       const apiRequest = httpMock.expectOne({ method: 'GET', url: expectedUrl });
-      apiRequest.flush(expectedPayload);
+      apiRequest.flush(searchPayload);
       httpMock.verify();
+    });
 
+    it('should return a result for movie specific search', (done) => {
+      const query = 'star wars';
+      const filter = 'movie';
+      const page = 1;
+
+      const expectedUrl = 'https://api.themoviedb.org/3/search/movie?'
+        + 'include_adult=false&page=1&query=star%20wars&language=en-US'
+        + '&api_key=224cff7f326dbbfde49d42869c0e1f1e';
+
+      apiService.searchMovies(query, filter, page)
+        .subscribe((data) => {
+          expect(data).toBe(searchPayload);
+          done();
+        });
+
+      const apiRequest = httpMock.expectOne({ method: 'GET', url: expectedUrl });
+      apiRequest.flush(searchPayload);
+      httpMock.verify();
+    });
+
+    it('should return a result for actor search', (done) => {
+      const query = 'star wars';
+      const filter = 'actor';
+      const page = 1;
+
+      const expectedUrl = 'https://api.themoviedb.org/3/search/person?'
+        + 'include_adult=false&page=1&query=star%20wars&language=en-US'
+        + '&api_key=224cff7f326dbbfde49d42869c0e1f1e';
+
+      apiService.searchMovies(query, filter, page)
+        .subscribe((data) => {
+          expect(data).toBe(searchPayload);
+          done();
+        });
+
+      const apiRequest = httpMock.expectOne({ method: 'GET', url: expectedUrl });
+      console.log(apiRequest.request.url);
+      apiRequest.flush(searchPayload);
+      httpMock.verify();
+    });
+
+    it('should default page when omitted', (done) => {
+      const query = 'star wars';
+      const filter = 'movie';
+
+      const expectedUrl = 'https://api.themoviedb.org/3/search/movie?'
+        + 'include_adult=false&page=1&query=star%20wars&language=en-US'
+        + '&api_key=224cff7f326dbbfde49d42869c0e1f1e';
+
+      apiService.searchMovies(query, filter)
+        .subscribe((data) => {
+          expect(data).toBe(searchPayload);
+          done();
+        });
+
+      const apiRequest = httpMock.expectOne({ method: 'GET', url: expectedUrl });
+      apiRequest.flush(searchPayload);
+      httpMock.verify();
+    });
+
+    it('should default filter and page when omitted', (done) => {
+      const query = 'star wars';
+
+      const expectedUrl = 'https://api.themoviedb.org/3/search/multi?'
+        + 'include_adult=false&page=1&query=star%20wars&language=en-US'
+        + '&api_key=224cff7f326dbbfde49d42869c0e1f1e';
+
+      apiService.searchMovies(query)
+        .subscribe((data) => {
+          expect(data).toBe(searchPayload);
+          done();
+        });
+
+      const apiRequest = httpMock.expectOne({ method: 'GET', url: expectedUrl });
+      apiRequest.flush(searchPayload);
+      httpMock.verify();
     });
 
   });
