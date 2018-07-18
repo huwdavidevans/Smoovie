4568e40ca4b8ec08f638f8eb1e677733fed3be0a
huwman <hevans@sorensonmedia.com>
2018-07-18 09:57:09 +0100

    internal refactor

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -16,14 +16,14 @@ export class SearchResultComponent implements OnInit, OnChanges {
   description: string;
   detailsPath: string;
 
-  @Input() record: any;
-  @Input() recordType: string;
+  @Input() item: any;
+  @Input() filterChoice: string;
   constructor() { }
 
   ngOnInit() { }
 
   ngOnChanges() {
-    if (!this.record) { return; }
+    if (!this.item) { return; }
     this.determineType();
     this.headline = this.getIdentity();
     this.description = this.getDescription();
@@ -32,33 +32,33 @@ export class SearchResultComponent implements OnInit, OnChanges {
   }
 
   private determineType() {
-    if (this.recordType === 'both') {
-      this.type = this.record.media_type ? this.record.media_type : 'person';
+    if (this.filterChoice === 'both') {
+      this.type = this.item.media_type ? this.item.media_type : 'person';
     }
-    if (this.recordType === 'actor') {
+    if (this.filterChoice === 'actor') {
       this.type = 'person';
     }
-    if (this.recordType === 'movie') {
+    if (this.filterChoice === 'movie') {
       this.type = 'movie';
     }
   }
 
   private getImage() {
     if (this.type !== 'person') {
-      return this.record.poster_path ? IMG_ENDPOINT + '/w200' + this.record.poster_path : '/assets/movie-generic.jpg';
+      return this.item.poster_path ? IMG_ENDPOINT + '/w200' + this.item.poster_path : '/assets/movie-generic.jpg';
     }
-    return this.record.profile_path ? IMG_ENDPOINT  + '/w200' + this.record.profile_path : '/assets/actor-generic.jpg';
+    return this.item.profile_path ? IMG_ENDPOINT  + '/w200' + this.item.profile_path : '/assets/actor-generic.jpg';
   }
 
     private getDescription() {
       if (this.type !== 'person') {
-        return this.record.overview;
+        return this.item.overview;
       }
 
-      if (this.record.known_for && this.record.known_for.length > 0) {
-        return this.record.name + ' is an actor known for their involvement in various shows.';
+      if (this.item.known_for && this.item.known_for.length > 0) {
+        return this.item.name + ' is an actor known for their involvement in various shows.';
       } else {
-        return this.record.name + ' is an actor, but that\'s all we know.';
+        return this.item.name + ' is an actor, but that\'s all we know.';
       }
     }
 
@@ -69,14 +69,14 @@ export class SearchResultComponent implements OnInit, OnChanges {
       if (this.type === 'tv') {
         return '#';
       }
-      return '/movie/' + this.record.id;
+      return '/movie/' + this.item.id;
     }
 
   private getIdentity() {
     if (this.type === 'movie') {
-      return this.record.title;
+      return this.item.title;
     } else {
-      return this.record.name;
+      return this.item.name;
     }
   }
 }
