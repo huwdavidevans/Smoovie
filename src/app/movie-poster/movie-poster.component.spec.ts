35d883dd3d7b724795706e446aa6f20aaf7150dd
huwman <hevans@sorensonmedia.com>
2018-07-19 00:06:20 +0100

    more green tests

--- src/app/movie-poster/movie-poster.component.spec.ts
+++ src/app/movie-poster/movie-poster.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { SimpleChange } from '@angular/core';
 import { MoviePosterComponent } from './movie-poster.component';
 
 describe('MoviePosterComponent', () => {
@@ -16,10 +16,40 @@ describe('MoviePosterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MoviePosterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('ngOnChanges', () => {
+
+    const myMovie = {
+      id: 90210,
+      title: 'The Towering Inferno',
+      poster_path: '/fffff.jpg'
+    };
+
+    beforeEach(() => {
+      component.movie = myMovie;
+      component.ngOnChanges({
+        movie: new SimpleChange(null, myMovie, true)
+      });
+      fixture.detectChanges();
+    });
+
+    it('should set id', () => {
+      expect(component.id).toEqual(90210);
+    });
+
+    it('should set imagePath', () => {
+      expect(component.imagePath).toEqual('http://image.tmdb.org/t/p/w200/fffff.jpg');
+    });
+
+    it('should set movieTitle', () => {
+      expect(component.movieTitle).toEqual('The Towering Inferno');
+    });
+
+  });
+
 });
