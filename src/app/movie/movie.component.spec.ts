c0129a1185d14f2d5fd60d1db5fe57a2f5543dc7
huwman <hevans@sorensonmedia.com>
2018-07-19 01:02:58 +0100

    movie tests green

--- src/app/movie/movie.component.spec.ts
+++ src/app/movie/movie.component.spec.ts
@@ -9,13 +9,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
 import { MovieComponent } from './movie.component';
+
+import { Observable, throwError } from 'rxjs';
+import 'rxjs/add/observable/of';
+import { ApiService } from '../tmdb-api.service';
 
 describe('MovieComponent', () => {
   let component: MovieComponent;
   let fixture: ComponentFixture<MovieComponent>;
+  let apiService;
 
   beforeEach(async(() => {
+
+    apiService = jasmine.createSpyObj('ApiService', ['getMovieById', 'getCreditsByMovieId', 'getSimilarMoviesByMovieId']);
+
     TestBed.configureTestingModule({
       declarations: [
         MovieComponent,
@@ -29,7 +38,12 @@ describe('MovieComponent', () => {
         MomentModule,
         HttpClientTestingModule
       ], providers: [
-        { provide: APP_BASE_HREF, useValue: '/'}
+        { provide: ApiService, useValue: apiService },
+        { provide: APP_BASE_HREF, useValue: '/' },
+        {
+          provide: ActivatedRoute,
+          useValue: {snapshot: {params: {'id': '90210'}}}
+        }
       ]
     })
     .compileComponents();
@@ -38,10 +52,103 @@ describe('MovieComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MovieComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    apiService = TestBed.get(ApiService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    let movieSpy;
+    let creditsSpy;
+    let similarMoviesSpy;
+
+    const expectedDetails = {
+      'genres': [
+        {
+          'id': 35,
+          'name': 'Comedy'
+        }
+      ],
+      'homepage': null,
+      'id': 8699,
+      'poster_path': '/hafNeozn8Q4K6enP2ux5MTMCHjH.jpg',
+      'release_date': '2004-07-09',
+      'tagline': 'They bring you the news so you don\'t have to get it yourself.',
+      'title': 'Anchorman: The Legend of Ron Burgundy'
+    };
+
+    const expectedCredits = {
+      'id': 8699,
+      'cast': [
+        {
+          'character': 'Ron Burgundy',
+          'credit_id': '52fe44b5c3a36847f80a59e3',
+          'name': 'Will Ferrell',
+          'profile_path': '/dGxt3uGPlUJKIfHYiLasnEgR90e.jpg'
+        }
+      ],
+      'crew': [
+        {
+          'department': 'Directing',
+          'job': 'Director',
+          'name': 'Adam McKay',
+          'profile_path': '/pwc6pHC5d3y8NVsphURkKK8y2fL.jpg'
+        }
+      ]
+    };
+
+    const similarMovies = { results: [{ title: 'Star Wars' }, { title: 'Krull' }, { title: 'Silent Running' }] };
+
+    beforeEach(() => {
+      movieSpy = apiService.getMovieById.and.callFake(() => {
+        return Observable.of(expectedDetails);
+      });
+
+      creditsSpy = apiService.getCreditsByMovieId.and.callFake(() => {
+        return Observable.of(expectedCredits);
+      });
+
+      similarMoviesSpy = apiService.getSimilarMoviesByMovieId.and.callFake(() => {
+        return Observable.of(similarMovies);
+      });
+    });
+
+    it('should set the movie details', () => {
+      component.ngOnInit();
+      expect(component.details).toEqual(expectedDetails);
+    });
+
+    it('should set the imagePath for the movie', () => {
+      component.ngOnInit();
+      expect(component.imagePath).toEqual('http://image.tmdb.org/t/p/w400/hafNeozn8Q4K6enP2ux5MTMCHjH.jpg');
+    });
+
+    it('should set the state to loaded', () => {
+      component.ngOnInit();
+      expect(component.state).toEqual('loaded');
+    });
+
+    it('should set the state to error', () => {
+      movieSpy = apiService.getMovieById.and.callFake(() => {
+        return throwError('Error');
+      });
+      component.ngOnInit();
+      expect(component.state).toEqual('error');
+    });
+
+    it('should set the movie details', () => {
+      component.ngOnInit();
+      const credits = { state: 'loaded', cast: expectedCredits.cast, crew: expectedCredits.crew };
+      expect(component.credits).toEqual(credits);
+    });
+
+    it('should set the similar movies', () => {
+      component.ngOnInit();
+      const expectedSimilarMovies = { state: 'loaded', movies: similarMovies.results };
+      expect(component.similarMovies).toEqual(expectedSimilarMovies);
+    });
+
   });
 });
